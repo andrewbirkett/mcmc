1f5f5315c34713ea32560d42beca8be3d7bb4ef4
Andrew Birkett <andy@nobugs.org>
2017-03-01 21:24:36 +0000

    Datapoints now encodes zeros separately, to allow ZeroInflated distributions to be handled efficiently.

--- src/org/nobugs/mcmc/Datapoints.java
+++ src/org/nobugs/mcmc/Datapoints.java
@@ -1,18 +1,36 @@
 package org.nobugs.mcmc;
 
-public class Datapoints {
-    private double[] xs;
+import cern.colt.list.DoubleArrayList;
 
+public class Datapoints {
+    private DoubleArrayList data;
+    private int zeros;
 
     public Datapoints(double... xs) {
-        this.xs = xs;
+        this.data = new DoubleArrayList();
+        for (double x : xs) {
+            if (x == 0) {
+                zeros++;
+            } else {
+                data.add(x);
+            }
+        }
     }
 
     public double[] getAll() {
-        return xs;
+        if (zeros > 0) throw new IllegalStateException("TODO: pad with zeros");
+        return data.elements();
+    }
+
+    public int getZeros() {
+        return zeros;
+    }
+
+    public DoubleArrayList getNonZeroValues() {
+        return data;
     }
 
     public int size() {
-        return xs.length;
+        return data.size() + zeros;
     }
 }
